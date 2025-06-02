e52f129e3f403150333f0aafad7f035d92141cf8
zyne <zynelau@gmail.com>
2025-06-02 16:01:28 +0800

    modified:   Air traffic management/traffic.py

--- Air traffic management/traffic.py
+++ Air traffic management/traffic.py
@@ -2,4 +2,4 @@ path = 'Air traffic management\\'
 file = open(path + 'current traffic.txt', "a")
 intended_landing_time = int(input("What is your intended landing time: "))
 flight_number = input("What is your flight number? ")
-print()
+traffic_info = f'{flight_number} | {intended_landing_time}'
