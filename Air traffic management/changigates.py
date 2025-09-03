23450874a1dd8a35f2c3d78bcbd340d815e3bdad
zayn_by <zynelau77@gmail.com>
2025-09-03 13:06:37 +0800

    modified:   Air traffic management/changigates.py

--- Air traffic management/changigates.py
+++ Air traffic management/changigates.py
@@ -1,5 +1,5 @@
 terminal_availability = {
-    'C11':True,"C12":True, "C13":True, "C14":True, "C15":True, "C16":True, "C17":True, "C18":True, "C19":True, "C20":True,
+    'C1':True,'C11':True,"C13":True, "C15":True,"C16":True, 'C17R':True,"C17":True, 'C17L':True,"C18":True, "C19":True, "C20":True,"C22":True,"C23":True,"C24":True,"C25":True,"C26":True,
     "D30":True, "D31":True, "D32":True, "D33":True, "D34":True, "D35":True, "D36":True, "D37":True, "D38":True, "D39":True, "D40":True, "D41":True,
     "E1":True, "E2":True, "E3":True, "E4":True, "E5":True, "E6":True, "E7":True, "E8":True, "E9":True, "E10":True,"E11":True, "E12":True, "E13":True, "E14":True, "E15":True, "E16":True, "E17":True, "E18":True, "E19":True, "E20":True,"E21":True, "E22":True, "E23":True, "E24":True, "E25":True, "E26":True, "E27":True, "E28":True,
     "F30":True, "F31":True, "F32":True, "F33":True, "F34":True, "F35":True, "F36":True, "F37":True, "F38":True, "F39":True, "F40":True,"F41":True, "F42":True, "F43":True, "F44":True, "F45":True, "F46":True, "F47":True, "F48":True, "F49":True, "F50":True,"F51":True, "F52":True, "F53":True, "F54":True, "F55":True, "F56":True, "F57":True, "F58":True, "F59":True, "F60":True,
@@ -7,3 +7,11 @@ terminal_availability = {
     "B1":True, "B2":True, "B3":True, "B4":True, "B5":True, "B6":True, "B7":True, "B8":True, "B9":True, "B10":True
 }
 
+terminal_acceptabilities = {
+    'C1':True,'C11':True,"C13":True, "C15":True,"C16":True, 'C17R':True,"C17":True, 'C17L':True,"C18":True, "C19":True, "C20":True,"C22":True,"C23":True,"C24":True,"C25":True,"C26":True,
+    "D30":True,"D32":True, "D34":True, "D35":True, "D36":True, "D37":True, "D38":True,"D40L":True, "D40":True, "D40R":True, "D41":True, "D42L":True,"D42":True, "D42R":True, "D44":True, "D46":True, "D47":True, "D48":True, "D49":True, 
+    "E1":True, "E2":True, "E3":True, "E4":True, "E6":True, "E7":True, "E8":True, "E10":True,"E11":True, "E12":True, "E22":True, "E24L":True, "E24":True, "E24R":True, "E26":True, "E27L":True, "E27":True, "E27R":True, "E28":True, 
+    "F30":True, "F31":True, "F32":True, "F33":True, "F34":True, "F35":True, "F36":True, "F37":True, "F38":True, "F39":True, "F40":True,"F41":True, "F42":True, "F43":True, "F44":True, "F45":True, "F46":True, "F47":True, "F48":True, "F49":True, "F50":True,"F51":True, "F52":True, "F53":True, "F54":True, "F55":True, "F56":True, "F57":True, "F58":True, "F59":True, "F60":True,
+    "A1":True, "A2":True, "A3":True, "A4":True, "A5":True, "A6":True, "A7":True, "A8":True, "A9":True, "A10":True,"A11":True, "A12":True, "A13":True, "A14":True, "A15":True, "A16":True, "A17":True, "A18":True, "A19":True, "A20":True, "A21":True,
+    "B1":True, "B2":True, "B3":True, "B4":True, "B5":True, "B6":True, "B7":True, "B8":True, "B9":True, "B10":True
+}
